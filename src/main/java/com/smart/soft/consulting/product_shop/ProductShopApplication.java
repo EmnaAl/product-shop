88bcd3c48bff258b9e70869a64684b2fdfd4badf
Emna Aloulou <emna.aloulou@sofrecom.com>
2024-11-07 21:09:52 +0100

    init project

---
+++ src/main/java/com/smart/soft/consulting/product_shop/ProductShopApplication.java
@@ -0,0 +1,13 @@
+package com.smart.soft.consulting.product_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductShopApplication.class, args);
+	}
+
+}
